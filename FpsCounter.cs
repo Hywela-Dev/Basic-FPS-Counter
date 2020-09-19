1c885f93c69b6c728e93d263bc4e60a82ee62a2f
Hywela-Dev <71149848+Hywela-Dev@users.noreply.github.com>
2020-09-19 16:29:55 +0100

    Add files via upload

---
+++ FpsCounter.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FpsCounter : MonoBehaviour
+{
+
+    public int FPS;
+    bool toggleFPS = true;
+
+    private void Update()
+    {
+        FPS = (int)(1 / Time.unscaledDeltaTime);
+    }
+
+    private void OnGUI()
+    {
+        if (!toggleFPS)
+        {
+            return;
+        }
+
+        GUI.Box(new Rect(10,10,50,25), FPS.ToString());
+
+    }
+
+}
